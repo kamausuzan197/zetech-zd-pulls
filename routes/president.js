857c63a2dfb438adce64fe9fe81cf3e6e0fba5ed
sankaire <kerenketepela@gmail.com>
2021-10-23 13:11:43 +0300

    feat: initial backend intergration

---
+++ routes/president.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+
+const Pusher = require("pusher")
+
+const pusher = new Pusher({
+    appId: "1272131",
+    key: "5eebcaa203111be8fb46",
+    secret: "07bc03ca2b5887b9804d",
+    cluster: "ap2",
+    useTLS: true
+  });
+
+router.get("/", (req, res) =>{
+    res.send("president")
+})
+
+router.post("/", (req, res) =>{
+    pusher.trigger("zt-poll", "zt-vote", {
+        points:1,
+        zt:req.body.zt
+    });
+    return res.json({success:true, message:'thank you for voting'})
+})
+
+module.exports = router
