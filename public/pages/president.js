c7ec2af37d7a9e79af539625f5763dd79c89d5f6
kamaususan197 <kamaususan197@gmail.com>
2021-10-29 19:46:01 +0300

    adding a comment

--- public/pages/president.js
+++ public/pages/president.js
@@ -2,6 +2,7 @@ const form = document.querySelector("#aspirants")
 
 //form event
 form.addEventListener('submit', (e) =>{
+  //when you click this button it responds by getting the data and initializing it as instructed 
     const choice = document.querySelector("input[name = zt]:checked").value
     const data = {zt:choice}
 
@@ -14,7 +15,7 @@ form.addEventListener('submit', (e) =>{
     }).then(res => res.json())
       .then(data => console.log(data))
       .catch(err => console.log(err))
-
+//e.is a function to desplay the defaul are of the page
     e.preventDefault()
 })
 //chart
