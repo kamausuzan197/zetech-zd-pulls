26d66b19092e1eba0d6b60792eed067b59e97a6e
kamaususan197 <kamaususan197@gmail.com>
2021-10-29 19:38:10 +0300

    adding a comment

--- public/main.js
+++ public/main.js
@@ -20,6 +20,7 @@ window.onload = function () {
     //humberger menu
     const menuBtn = document.querySelector(".humbuger")
     const mobileNav = document.querySelector(".mobile-nav")
+    
     menuBtn.addEventListener('click', ()=>{
       menuBtn.classList.toggle("is-active")
       mobileNav.classList.toggle("is-active")
