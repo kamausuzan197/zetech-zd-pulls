a7a1fccb50bba0706cfec1de39d3a5b831e24cdb
sankaire <kerenketepela@gmail.com>
2021-11-09 16:33:07 +0300

    feat: rm cusual scripts

--- public/main.js
+++
@@ -1,8 +0,0 @@
-//humberger
-const menuBtn = document.querySelector(".humbuger")
-const mobileNav = document.querySelector(".mobile-nav")
-
-menuBtn.addEventListener('click', ()=>{
-  menuBtn.classList.toggle("is-active")
-  mobileNav.classList.toggle("is-active")
-})
